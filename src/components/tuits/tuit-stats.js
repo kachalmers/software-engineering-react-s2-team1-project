f99a21a09cbec829d6339e823b2447e397296297
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-03-25 18:57:09 -0400

    trying fill thumbs up only if logged in user likes tuit

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as likesService from "../../services/likes-service";
 
 const TuitStats = ({tuit, likeTuit = () => {}}) => {
     return (
@@ -14,11 +15,14 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
+                  // If I've liked the tuit
                   tuit.stats && tuit.stats.likes > 0 &&
+                  likesService.userLikesTuit("me", tuit._id) &&
                   <i className="fas fa-thumbs-up me-1" style={{color: 'blue'}}></i>
               }
               {
-                  tuit.stats && tuit.stats.likes <= 0 &&
+                  tuit.stats && //tuit.stats.likes <= 0 &&
+                  !(likesService.userLikesTuit("me", tuit._id)) &&
                   <i className="far fa-thumbs-up me-1"></i>
               }
             {tuit.stats && tuit.stats.likes}
