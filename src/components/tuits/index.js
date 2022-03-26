d2c37157a7bdf5feb37bb74078dc835aa17ae38c
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-03-26 08:13:53 -0400

    added dislikes functionality to index.js

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
 import * as likesService from "../../services/likes-service";
+import * as dislikesService from "../../services/dislikes-service";
 import * as service from "../../services/tuits-service";
+
 const Tuits = ({tuits = [], refreshTuits}) => {
     const likeTuit = (tuit) =>
         likesService.userLikesTuit("me", tuit._id)
             .then(refreshTuits)
-            .catch(e => alert(e))
+            .catch(e => alert(e));
+
+    const dislikeTuit = (tuit) =>
+        dislikesService.userDislikesTuit("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e));
+
     const deleteTuit = (tid) =>
         service.deleteTuit(tid)
             .then(refreshTuits);
@@ -20,6 +28,7 @@ const Tuits = ({tuits = [], refreshTuits}) => {
                   <Tuit key={tuit._id}
                         deleteTuit={deleteTuit}
                         likeTuit={likeTuit}
+                        dislikeTuit={dislikeTuit}
                         tuit={tuit}/>)
             }
           </ul>
