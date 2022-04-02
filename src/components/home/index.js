7436829a20cdd82b103d2f59404fce7a2153af23
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 18:02:31 -0400

    Reformatted error status message. Minor updates.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,81 +2,79 @@ import React from "react";
 import Tuits from "../tuits";
 import * as service from "../../services/tuits-service";
 import {useEffect, useState} from "react";
-import {useLocation, useParams} from "react-router-dom";
-import * as authService from "../../services/security-service";
+import * as authService from "../../services/auth-service";
 
 const Home = () => {
-  const location = useLocation();
-  const {uid} = useParams();
   const [tuits, setTuits] = useState([]);
   const [tuit, setTuit] = useState('');
-  const userId = uid;
-  const findTuits = () =>
-      service.findAllTuits()
-        .then(tuits => setTuits(tuits));
-
+  const findTuits = () => {
+    return service.findAllTuits().then(tuits => { setTuits(tuits) })
+  }
   const [profile, setProfile] = useState({});
 
   useEffect(() => {
-    let isMounted = true;
     try {
       authService.profile()
           .then((user) => {
             if (user) {
               setProfile(user);
-              findTuits();
             }
           });
     } catch (e) {
     }
-    return () => {isMounted = false;}
+    findTuits();
   }, []);
 
-  const createTuit = () =>
-      service.createTuit('me', {tuit})
-          .then(findTuits)
-          .catch(e=>{
-            alert("Error: Request failed with status code "
-                  + e.response.status + ". "
-                  + e.response.data);
-          });
+  const createTuit = () => {
+    if (tuit === '') {
+      alert("Cannot post empty tuit!")
+    } else {
+      service.createTuitByUser(profile._id, {tuit}).then(findTuits);
+      setTuit("");
+    }
+  }
+
   return(
     <div className="ttr-home">
       <div className="border border-bottom-0">
         <h4 className="fw-bold p-2">Home Screen</h4>
-        <div className="d-flex">
-          <div className="p-2">
-            <img className="ttr-width-50px rounded-circle"
-                 src="../images/nasa-logo.jpg"/>
-          </div>
-          <div className="p-2 w-100">
-            <textarea
-                onChange={(e) =>
-                    setTuit(e.target.value)}
-              placeholder="What's happening?"
-              className="w-100 border-0"></textarea>
-            <div className="row">
-              <div className="col-10 ttr-font-size-150pc text-primary">
-                <i className="fas fa-portrait me-3"></i>
-                <i className="far fa-gif me-3"></i>
-                <i className="far fa-bar-chart me-3"></i>
-                <i className="far fa-face-smile me-3"></i>
-                <i className="far fa-calendar me-3"></i>
-                <i className="far fa-map-location me-3"></i>
-              </div>
-              <div className="col-2">
-                <a onClick={createTuit}
-                   className={`btn btn-primary rounded-pill fa-pull-right
-                                fw-bold ps-4 pe-4`}>
-                  Tuit
-                </a>
+        {
+          profile._id &&
+          <div className="d-flex">
+            <div className="p-2">
+              <img className="ttr-width-50px rounded-circle"
+                   src="../images/nasa-logo.jpg"/>
+            </div>
+            <div className="p-2 w-100">
+              <textarea
+                  value={tuit}
+                  onChange={(e) =>
+                      setTuit(e.target.value)}
+                placeholder="What's happening?"
+                className="w-100 border-0">
+              </textarea>
+              <div className="row">
+                <div className="col-10 ttr-font-size-150pc text-primary">
+                  <i className="fas fa-portrait me-3"></i>
+                  <i className="far fa-gif me-3"></i>
+                  <i className="far fa-bar-chart me-3"></i>
+                  <i className="far fa-face-smile me-3"></i>
+                  <i className="far fa-calendar me-3"></i>
+                  <i className="far fa-map-location me-3"></i>
+                </div>
+                <div className="col-2">
+                  <a onClick={createTuit}
+                     className={`btn btn-primary rounded-pill fa-pull-right
+                                  fw-bold ps-4 pe-4`}>
+                    Tuit
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
       </div>
-      <Tuits tuits={tuits}
-             refreshTuits={findTuits}/>
+      <Tuits tuits={tuits} refreshTuits={findTuits}/>
     </div>
   );
 };
