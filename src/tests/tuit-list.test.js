a7a8cd0a7d9500d176305b1038646ecf93f9332f
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 20:28:41 -0400

    Updated tuit-list.test.js to work with new environment

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -2,31 +2,18 @@ import Index from "../components/tuits";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {api, findAllTuits, deleteTuit, deleteTuitByTuitText, createTuitByUser} from "../services/tuits-service";
-import axios from "axios";
 import {createUser, deleteUsersByUsername} from "../services/users-service";
-
-// jest.mock('axios');
 
 const MOCKED_TUITS = [
     {
-        tuit: "TestAlice",
+        tuit: "alice's tuit",
         postedBy: {
             username: "Alice",
             password: "alice123",
-            email: "alice@weyland.com",
-            _id: "123"
+            email: "alice@wonderland.com",
+            _id: "1122"
         },
-        _id: "12"
-    },
-    {
-        tuit: "bob's tuit",
-        postedBy: {
-            username: "Bob",
-            password: "bob123",
-            email: "bob@123.com",
-            _id: "234"
-        },
-        _id: "34"
+        _id: "111"
     },
     {
         tuit: "charlie's tuit",
@@ -34,10 +21,20 @@ const MOCKED_TUITS = [
             username: "Charlie",
             password: "charlie123",
             email: "charlie@123.com",
-            _id: "345"
+            _id: "5566"
         },
-        _id: "45"
-    }
+        _id: "333"
+    },
+    {
+        tuit: "bob's tuit",
+        postedBy: {
+            username: "Bob",
+            password: "bob123",
+            email: "bob@123.com",
+            _id: "3344"
+        },
+        _id: "222"
+    },
 ];
 
 test('tuit list renders static tuit array', () => {
@@ -46,84 +43,99 @@ test('tuit list renders static tuit array', () => {
             <Index tuits={MOCKED_TUITS} deleteTuit={deleteTuit}/>
         </HashRouter>
     )
-    MOCKED_TUITS.map(eachTuit => {
-        let name = eachTuit.postedBy.username
-        // substring match, ignore case, same as /alice/i
-        const nameElements = screen.getAllByText(name, {exact: false});
-        const tuitElements = screen.getAllByText(eachTuit.tuit, {exact: false});
-        nameElements.forEach(e => expect(e).toBeInTheDocument());
+    MOCKED_TUITS.map(tuit => {
+        // Expect username to be found in the document
+        let name = tuit.postedBy.username;
+        const usernameElements = screen.getAllByText(name, {exact: false});
+        usernameElements.forEach(e => expect(e).toBeInTheDocument());
+
+        // Expect tuit text to be found in the document
+        const tuitElements = screen.getAllByText(tuit.tuit, {exact: false});
         tuitElements.forEach(e => expect(e).toBeInTheDocument());
     })
 });
 
 describe('tuit list renders async', ()=> {
 
-    // samples tuits we'll insert to then retrieve
-    const tuitContents = [
-        "adam's Tuit 1", "adam's Tuit 2", "adam's Tuit 3"
+    const tuitText = [
+        "janice's test tuit 1",
+        "janice's test tuit 2",
+        "janice's test tuit 3"
     ]
 
-    // sample user to insert as the author of tuits
     const user = {
-        username: 'adam_smith',
-        password: 'adam123',
-        email: 'wealth@nations.com'
+        username: 'janice_joplin',
+        password: 'janice123',
+        email: 'pieceof@myheart.com'
     };
 
-    // setup data before test
+    // Set up tests
     beforeAll(() => {
         let promises = [];
-        // delete the user we inserted
         promises.push(deleteUsersByUsername(user.username));
-        // delete tuits we inserted
-        tuitContents.map(content => {
-            promises.push(deleteTuitByTuitText(content));
+        tuitText.map(text => {
+            promises.push(deleteTuitByTuitText(text));
         })
         return Promise.all(promises);
     })
 
-    // clean up after test runs
+    // Clean up tests
     afterAll(()=> {
         let promises = []
-        // delete the user we inserted
         promises.push(deleteUsersByUsername(user.username));
-        // delete tuits we inserted
-        tuitContents.map((content) => {
-            let deletePromise = deleteTuitByTuitText(content);
+        tuitText.map((text) => {
+            let deletePromise = deleteTuitByTuitText(text);
             promises.push(deletePromise)
         })
         return Promise.all(promises)
     })
 
     test('tuit list renders async', async () => {
-        const author = await createUser(user);
-        let promises = [];
-        tuitContents.map(content => {
-            promises.push(createTuitByUser(author._id, {tuit: content, postedOn: "2022-03-09T00:00:00.000Z"}));
+        const author = await createUser(user);  // Create new user
+
+        let promises = [];  // Initialize an empty array of promises
+
+        // Create a new tuit per tuit text in tuitText array
+        tuitText.map(text => {
+            promises.push(
+                createTuitByUser(author._id,
+                                 {
+                                     tuit: text,
+                                     postedOn: "2022-03-24T00:00:00.000Z"
+                                 }
+                                 )
+            );
         })
-        await Promise.all(promises);
-        const tuits = await findAllTuits();
+
+        await Promise.all(promises);    // Wait for all promises to be fulfilled
+
+        const tuits = await findAllTuits(); // Find all tuits
+
+        // Render page with tuits
         render(
             <HashRouter>
                 <Index tuits={tuits}/>
             </HashRouter>
         )
 
-        // Knowing there are 3 tuits by adam
-        tuitContents.forEach(content => {
-            const tuitElements = screen.getAllByText(content, {exact: false});
+        // For each tuit text...
+        tuitText.forEach(text => {
+            // Check the tuit text exists in the document
+            const tuitElements = screen.getAllByText(text, {exact: false});
             tuitElements.forEach(e => expect(e).toBeInTheDocument());
         })
-        // knowing there is adman names
-        let nameElements = screen.getAllByText(user.username, {exact: false});
-        nameElements.forEach((e) => expect(e).toBeInTheDocument());
 
-        // following what professor did
-        // we know there is a nasa tuit posted by nasa
-        const linkElements = screen.getAllByText(/nasa/i);
+        // Expect test username to be in the document
+        let usernameElements = screen.getAllByText(user.username, {exact: false});
+        usernameElements.forEach((e) => expect(e).toBeInTheDocument());
+
+        // Get janice's tuit and expect it to be in the document
+        const linkElements = screen.getAllByText(/janice's test tuit 1/i);
         linkElements.forEach((l) => expect(l).toBeInTheDocument());
-        let tuitElement = screen.getByText(/@NASAPersevere/i);
-        expect(tuitElement).toBeInTheDocument();
+
+        // Get janice's username and expect it to be in the document
+        let tuitElement = screen.getAllByText(/@janice_joplin/i);
+        tuitElement.forEach((l) => expect(l).toBeInTheDocument());
     })
 })
 
@@ -136,18 +148,21 @@ test('tuit list renders mocked', async () => {
 
     mock.mockRestore();  // restore original implementation
 
+    // Render tuits on page
     render(
         <HashRouter>
             <Index tuits={tuits}/>
         </HashRouter>
     )
 
-    MOCKED_TUITS.map(eachTuit => {
-        let name = eachTuit.postedBy.username
-        // substring match, ignore case, same as /alice/i
-        const nameElements = screen.getAllByText(name, {exact: false});
-        const tuitElements = screen.getAllByText(eachTuit.tuit, {exact: false});
-        nameElements.forEach(e => expect(e).toBeInTheDocument());
+    MOCKED_TUITS.map(tuit => {
+        // Expect username to be found in the document
+        let name = tuit.postedBy.username;
+        const usernameElements = screen.getAllByText(name, {exact: false});
+        usernameElements.forEach(e => expect(e).toBeInTheDocument());
+
+        // Expect tuit text to be found in the document
+        const tuitElements = screen.getAllByText(tuit.tuit, {exact: false});
         tuitElements.forEach(e => expect(e).toBeInTheDocument());
     })
 });
