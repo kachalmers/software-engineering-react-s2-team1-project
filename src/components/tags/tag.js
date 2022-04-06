7e3a87bf2fb5b91964711bcaf1c6ea9592d09f23
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-06 17:16:19 -0400

    Added navigation to tuitsbytag page, removed unused imports

--- src/components/tags/tag.js
+++ src/components/tags/tag.js
@@ -2,25 +2,24 @@
  * @file Implement Tag component for displaying each tag
  */
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Tag = ({tag}) => {
-
-
+    const navigate = useNavigate();
 
     return (
         <div className="row">
             <div className="col-6">
-                <a onClick={console.log("Clicked")}
+                <a href="#/explore/tuitsbytag"
                    className={`btn btn-primary rounded-pill fa-pull-right
                                   fw-bold ps-4 pe-4`}>
                     {tag.tag}
                 </a>
             </div>
             <div className="col-5 ttr-font-size-150pc text-primary">
-                <h>
+                <p>
                     {"Tag count: "+tag.count}
-                </h>
+                </p>
             </div>
         </div>
     );
