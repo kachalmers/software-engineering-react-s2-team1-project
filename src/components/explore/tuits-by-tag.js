d6d1e9ad9b5d47813638c185a0ad3c3fc91de076
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-10 17:07:45 -0400

    Added KC's edits to prevent merge issues

--- src/components/explore/tuits-by-tag.js
+++ src/components/explore/tuits-by-tag.js
@@ -4,18 +4,66 @@ import Tuits from "../tuits"
 
 const TuitsByTag = () => {
     const [tuits, setTuits] = useState([]);
+    const [tempTag, setTempTag] = useState('');
+    const [tag, setTag] = useState('');
+
+    // Currently sets tuits to be shown back to an empty list of tuits
+    // TBD what we actually want to show here,
+    // We can decide when we get the backend hooked-up and working
+    const findAllTuits = () => {
+        /*
+        // Find all tuits
+        service.findAllTuits()
+            .then(tuits => {
+                setTuits(tuits);
+            })
+
+         */
+        setTuits([]);   // set tuits to empty array
+        setTag(tempTag);    // Set tag to what was entered
+    };
+
     const findTuitsWithTag = () => {
-        // #KAC-findTuitsWithTag
+        // #KAC
+        // findAllTuits in next line to be changed to findTuitsWithTag({tag})
         service.findAllTuits()
             .then(tuits => {
                 setTuits(tuits);
             })
     };
 
+    // When we first load the page...
     useEffect(() => {
-        findTuitsWithTag()
+        setTuits([]);   // Show no tuits...
+        /*
+        Note: this can later be changed to show all tuits with any tag.
+        This can later be discussed by the team how we want to present this
+         */
     }, []);
 
+    // Whenever a keyboard button is clicked...
+    const handleKeyDown = (event) => {
+        /*
+        If the 'Enter' key was pressed and there's something in the input
+        box...
+         */
+        if (event.key === 'Enter') {
+            if (tempTag !== "") {
+                setTag(tempTag);    // Set tag to what was entered
+                findTuitsWithTag(); //
+                setTempTag("");
+            } else {
+                findAllTuits(); // currently shows no tuits right now
+            }
+
+        }
+
+    }
+
+    // Log won't pick up on updates to states of objects until right before the return...
+    // printing here to show value of tag
+    // https://jsramblings.com/are-you-logging-the-state-immediately-after-updating-it-heres-why-that-doesnt-work/
+    console.log(JSON.parse(JSON.stringify("before return: "+tag)));
     return (
         <div className="ttr-whats-happening p-0">
             <div className="ttr-search position-relative">
