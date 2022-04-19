1018f483d2106031bd277eba25d1a54c7212733d
nolanmatt34 <nolan.matt@northeastern.edu>
2022-04-19 12:27:14 -0400

    Added button functionality to the screen

--- src/components/tuits/update-tuit.js
+++ src/components/tuits/update-tuit.js
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
 import * as service from "../../services/tuits-service"
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import Tuit from "./tuit";
+import {updateTuit} from "../../services/tuits-service";
 
 const TuitScreen = () => {
     const [tuit, setTuit] = useState({});
@@ -14,6 +15,13 @@ const TuitScreen = () => {
         <div>
             <textarea />
             <Tuit tuit={tuit}/>
+            <i onClick = {()=>updateTuit(tid, tuit)}
+                className = "float-end tuit-button me-1">
+                Update
+            </i>
+            <Link to={`/`}>
+                <i className="float-end tuit-button me-1"> Cancel </i>
+            </Link>
         </div>
     );
 };
